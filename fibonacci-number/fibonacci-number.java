4c5f0e0afd19ee035cd8e2c39853588ae1e7a8e2
aj7521 <43270371+aj7521@users.noreply.github.com>
2023-05-07 23:04:22 +0530

    Time: 0 ms (100.00%) | Memory: 39.7 MB (32.97%) - LeetSync

---
+++ fibonacci-number/fibonacci-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int fib(int n) {
+        int prev = 1;
+        int prev2 = 0;
+        if(n==0) return prev2;
+        for(int i=2; i<=n; i++)
+        {
+            int cur = prev + prev2;
+            prev2 = prev;
+            prev = cur;
+        }
+        return prev;
+    }
+}
