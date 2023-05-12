ef9cc86c5e646917f67958a22666ccf5a1533c99
aj7521 <43270371+aj7521@users.noreply.github.com>
2023-05-12 18:54:10 +0530

    Time: 14 ms (7.82%) | Memory: 40.8 MB (6.98%) - LeetSync

---
+++ house-robber-ii/house-robber-ii.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n==1) return nums[0];
+        int arr1[] = new int[n-1];
+        int arr2[] = new int[n-1];
+        for(int i=0; i<nums.length-1; i++)
+        {
+            arr1[i] = nums[i];
+            System.out.print(arr1[i] + " ");
+        }
+        int p = 0;
+        System.out.println();
+        for(int i=1; i<nums.length; i++)
+        {
+            arr2[p] = nums[i];
+            System.out.print(arr2[p] + " ");
+            p++;
+        }
+        return Math.max(check(arr1), check(arr2));
+    }
+
+    public int check(int arr[])
+    {
+        int prev = arr[0];
+        int prev2 = 0;
+        for(int i=1; i<arr.length; i++)
+        {
+            int take = arr[i];
+            if(i>1) take+=prev2;
+            int nontake = 0 + prev;
+
+            int curi = Math.max(take, nontake);
+            prev2 = prev;
+            prev = curi;
+        }
+        return prev;
+    }
+}
