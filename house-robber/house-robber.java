bc169d9991062e058652416d1bee5f8bfc86950b
aj7521 <43270371+aj7521@users.noreply.github.com>
2023-05-12 18:44:34 +0530

    Time: 0 ms (100.00%) | Memory: 39.7 MB (90.03%) - LeetSync

--- house-robber/house-robber.java
+++ house-robber/house-robber.java
@@ -1,23 +1,23 @@
 class Solution {
     public int rob(int[] nums) {
-        int dp[] = new int[nums.length];
-        return check(nums, dp);
+        return check(nums);
     }
 
-    public int check(int arr[], int dp[])
+    public int check(int arr[])
     {
-        dp[0] = arr[0];
-        int neg = 0;
-
+        int prev = arr[0];
+        int prev2 = 0;
         for(int i=1; i<arr.length; i++)
         {
             int take = arr[i];
-            if(i>1) take+=dp[i-2];
-            int nontake = 0 + dp[i-1];
+            if(i>1) take+=prev2;
+            int nontake = 0 + prev;
 
-            dp[i] = Math.max(take, nontake);
+            int curi = Math.max(take, nontake);
+            prev2 = prev;
+            prev = curi;
         }
-        return dp[arr.length-1];
+        return prev;
     }
     
 }
