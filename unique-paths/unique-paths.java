bc62aa99e34b3d708db206cd07e47ce938089d69
aj7521 <43270371+aj7521@users.noreply.github.com>
2023-05-13 16:20:56 +0530

    Time: 0 ms (100.00%) | Memory: 39.3 MB (84.95%) - LeetSync

--- unique-paths/unique-paths.java
+++ unique-paths/unique-paths.java
@@ -1,24 +1,34 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int dp[][] = new int [m][n];
+        return countPaths(m, n, dp);
+    }
+
+    public int countPaths(int m, int n, int dp[][])
+    {
         for(int i=0; i<m; i++)
         {
             for(int j=0; j<n; j++)
             {
-                dp[i][j] = -1;
+                if(i==0 && j==0){
+                    dp[i][j] = 1;
+                }
+                else
+                {
+                    int left = 0, up = 0;
+                    if(j>0) {
+                        left = dp[i][j-1];
+                    }
+                    if(i>0)
+                    {
+                        up = dp[i-1][j];
+                    }
+                    dp[i][j] = left + up;
+                }
             }
         }
-        return countPaths(m-1, n-1, dp);
-    }
+        
 
-    public int countPaths(int i, int j, int dp[][])
-    {
-        if(i==0 && j==0) return 1;
-        if(i<0 || j<0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        int left = countPaths(i, j-1, dp);
-        int up = countPaths(i-1, j, dp);
-
-        return dp[i][j] = left+up;
+        return dp[m-1][n-1];
     }
 }
