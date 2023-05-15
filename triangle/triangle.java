2fa26e06c03d805086ad77caed5fb536035a75da
aj7521 <43270371+aj7521@users.noreply.github.com>
2023-05-16 01:23:44 +0530

    Time: 4 ms (52.97%) | Memory: 42.3 MB (64.21%) - LeetSync

--- triangle/triangle.java
+++ triangle/triangle.java
@@ -7,16 +7,33 @@ class Solution {
                 dp[i][j] = -1;
             }
         }
-        return count(0, 0, n, t, dp);
+        return count(n, t, dp);
     }
 
-    public int count(int i, int j, int n, List<List<Integer>> t, int dp[][])
-    {
-        if(i==n-1) return t.get(i).get(j);
-        if(dp[i][j] != -1) return dp[i][j];
-        int down = count(i+1, j, n, t, dp);
-        int slant = count(i+1, j+1, n, t, dp);
+    // public int count(int i, int j, int n, List<List<Integer>> t, int dp[][])
+    // {
+    //     if(i==n-1) return t.get(i).get(j);
+    //     if(dp[i][j] != -1) return dp[i][j];
+    //     int down = count(i+1, j, n, t, dp);
+    //     int slant = count(i+1, j+1, n, t, dp);
         
-        return dp[i][j] = Math.min(down, slant) + t.get(i).get(j);
+    //     return dp[i][j] = Math.min(down, slant) + t.get(i).get(j);
+    // }
+
+    public int count(int n, List<List<Integer>> t, int dp[][])
+    {
+        for(int i=n-1; i>=0; i--){
+            for(int j=i; j>=0; j--){
+                if(i==n-1) dp[i][j] = t.get(i).get(j); 
+                else
+                {
+                    int down = 0, slant = 0;
+                    down = dp[i+1][j];
+                    slant = dp[i+1][j+1];
+                    dp[i][j] = Math.min(down, slant) + t.get(i).get(j);
+                }
+            }
+        }
+        return dp[0][0];
     }
 }
