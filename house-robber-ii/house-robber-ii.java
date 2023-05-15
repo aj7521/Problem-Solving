093c4f7125dcd98cb1a3da792e6f35e7410f678c
aj7521 <43270371+aj7521@users.noreply.github.com>
2023-05-15 16:48:59 +0530

    Time: 13 ms (8.03%) | Memory: 41.1 MB (7.23%) - LeetSync

--- house-robber-ii/house-robber-ii.java
+++ house-robber-ii/house-robber-ii.java
@@ -24,15 +24,13 @@ class Solution {
     {
         int prev = arr[0];
         int prev2 = 0;
-        for(int i=1; i<arr.length; i++)
-        {
-            int take = arr[i];
-            if(i>1) take+=prev2;
-            int nontake = 0 + prev;
-
-            int curi = Math.max(take, nontake);
+        for(int i=1; i<arr.length; i++){
+            int np = prev;
+            int p = arr[i];
+            if(i>1) p+=prev2;
+            int cur = Math.max(np, p);
             prev2 = prev;
-            prev = curi;
+            prev = cur;
         }
         return prev;
     }
