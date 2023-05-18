316585f91a1033cb8db824ae9ab8f5b550d6f079
aj7521 <43270371+aj7521@users.noreply.github.com>
2023-05-18 17:19:34 +0530

    Time: 20 ms (47.27%) | Memory: 42.7 MB (36.94%) - LeetSync

--- coin-change/coin-change.java
+++ coin-change/coin-change.java
@@ -1,26 +1,22 @@
 class Solution {
-    public int coinChange(int[] coins, int amount) {
-        int dp[][] = new int[coins.length][amount+1];
-        for(int row[]: dp){
-            Arrays.fill(row, -1);
+    public int coinChange(int[] coins, int tar) {
+        int dp[][] = new int[coins.length][tar+1];
+        int n = coins.length;
+        for(int i=0; i<tar+1; i++){
+            if(i%coins[0]==0){
+                dp[0][i] = i/coins[0];
+            }
+            else dp[0][i] = (int)(1e9);
         }
-        int val = coin(coins.length-1, coins, amount, dp);
-        if(val >= (int)(1e9)){
-            return -1;
-        }
-        return val;
-    }
 
-    public int coin(int i, int coins[], int t, int dp[][]){
-        if(i==0){
-            if(t%coins[i]==0) return t/coins[i];
-            else return (int)(1e9);
+        for(int i=1; i<n; i++){
+            for(int j=0; j<=tar; j++){
+                int np = dp[i-1][j];
+                int p = Integer.MAX_VALUE;
+                if(coins[i]<=j) p = 1 + dp[i][j-coins[i]];
+                dp[i][j] = Math.min(p, np);
+            }
         }
-        if(dp[i][t]!=-1) return dp[i][t];
-        int np = coin(i-1, coins, t, dp);
-        int p = Integer.MAX_VALUE;
-        if(coins[i]<=t) p = 1 + coin(i, coins, t-coins[i], dp);
-
-        return dp[i][t] = Math.min(p, np);
+        return dp[n-1][tar]>=(int)(1e9)?-1:dp[n-1][tar];
     }
 }
