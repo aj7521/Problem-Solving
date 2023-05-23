0ca03cb70c55d1c1d8f3728fa9af526bccf56722
aj7521 <43270371+aj7521@users.noreply.github.com>
2023-05-24 03:32:29 +0530

    Time: 24 ms (19.00%) | Memory: 47.2 MB (81.80%) - LeetSync

--- distinct-subsequences/distinct-subsequences.java
+++ distinct-subsequences/distinct-subsequences.java
@@ -3,25 +3,39 @@ class Solution {
         int n = a.length();
         int m = b.length();
         int dp[][] = new int[n+1][m+1];
-        for(int i=0; i<=n; i++){ 
-            for(int j=0; j<=m; j++){
-                dp[i][j] = -1;
+        // for(int i=0; i<=n; i++){ 
+        //     for(int j=0; j<=m; j++){
+        //         dp[i][j] = -1;
+        //     }
+        // }
+        // return count(n, m, a, b, dp);
+        for(int i=0; i<=n; i++){
+            dp[i][0] = 1;
+        }
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                int np = dp[i-1][j];
+                int p = 0;
+                if(a.charAt(i-1)==b.charAt(j-1)){
+                    p = dp[i-1][j-1];
+                }
+                dp[i][j] = p + np;
             }
         }
-        return count(n-1, m-1, a, b, dp);
+        return dp[n][m];
     }
 
     public int count(int i, int j, String a, String b, int dp[][]){
-        if(j<0){
+        if(j==0){
             return 1;
         }
-        if(i<0){
+        if(i==0){
             return 0;
         }
         if(dp[i][j]!=-1) return dp[i][j];
         int np = count(i-1, j, a, b, dp);
         int p = 0;
-        if(a.charAt(i)==b.charAt(j)){
+        if(a.charAt(i-1)==b.charAt(j-1)){
             p = count(i-1, j-1, a, b, dp);
         }
         return dp[i][j] = p + np;
