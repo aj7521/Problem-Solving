ee27889588734751ef5af52f620c71fa3e879dff
aj7521 <43270371+aj7521@users.noreply.github.com>
2023-05-25 05:09:40 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (12.23%) - LeetSync

--- best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,47 +1,12 @@
 class Solution {
     public int maxProfit(int[] arr) {
         int n = arr.length;
-        int dp[] = new int[2];
-        // for(int rows[]: dp){
-        //     Arrays.fill(rows, -1);
-        // }
-        // return maxP(0, prices, 1, dp);
-        dp[0] = dp[1] = 0;
+        int b = 0, nb = 0;
         int profit = 0;
         for(int i=n-1; i>=0; i--){
-            for(int j=0; j<2; j++){
-                if(j==1){
-                    int take = dp[0] - arr[i];
-                    int ntake = dp[1];
-                    profit = Math.max(take, ntake);
-                }
-                else{
-                    int take = dp[1] + arr[i];
-                    int ntake = dp[0];
-                    profit = Math.max(take, ntake);
-                }
-                dp[j] = profit;
-            }
+            nb = Math.max(b + arr[i], nb);
+            b = Math.max(nb - arr[i], b);
         }
-        return dp[1];
-    }
-
-    public int maxP(int i, int arr[], int buy, int dp[][]){
-        if(i==arr.length){
-            return 0;
-        }
-        if(dp[i][buy]!=-1) return dp[i][buy];
-        int profit = 0;
-        if(buy==1){
-            int take = maxP(i+1, arr, 0, dp) - arr[i];
-            int ntake = maxP(i+1, arr, 1, dp);
-            profit = Math.max(take, ntake);
-        }
-        else{
-            int take = maxP(i+1, arr, 1, dp) + arr[i];
-            int ntake = maxP(i+1, arr, 0, dp);
-            profit = Math.max(take, ntake);
-        }
-        return profit;
+        return b;
     }
 }
