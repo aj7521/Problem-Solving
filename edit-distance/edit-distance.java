e3899d3b0c68b7d5f76715e8011e6c2ce150901b
aj7521 <43270371+aj7521@users.noreply.github.com>
2023-05-24 22:26:22 +0530

    Time: 7 ms (27.54%) | Memory: 44 MB (9.07%) - LeetSync

--- edit-distance/edit-distance.java
+++ edit-distance/edit-distance.java
@@ -5,16 +5,16 @@ class Solution {
         for(int row[]: dp){
             Arrays.fill(row, -1);
         }
-        return count(n-1, m-1, word1, word2, dp);
+        return count(n, m, word1, word2, dp);
     }
 
     public int count(int i, int j, String a, String b, int dp[][]){
-        if(i<0) return j+1;
-        if(j<0) return i+1;
+        if(i==0) return j;
+        if(j==0) return i;
 
         if(dp[i][j]!=-1) return dp[i][j];
 
-        if(a.charAt(i)==b.charAt(j)){
+        if(a.charAt(i-1)==b.charAt(j-1)){
             return 0 + count(i-1, j-1, a, b, dp);
         }
         int del = 1 + count(i-1, j, a, b, dp);
