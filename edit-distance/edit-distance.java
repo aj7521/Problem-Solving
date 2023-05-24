9d0d56e2897527206151fdcf7f81da2e38533ca5
aj7521 <43270371+aj7521@users.noreply.github.com>
2023-05-24 22:24:48 +0530

    Time: 7 ms (27.54%) | Memory: 43.6 MB (12.54%) - LeetSync

---
+++ edit-distance/edit-distance.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int n = word1.length(), m = word2.length();
+        int dp[][] = new int[n+1][m+1];
+        for(int row[]: dp){
+            Arrays.fill(row, -1);
+        }
+        return count(n-1, m-1, word1, word2, dp);
+    }
+
+    public int count(int i, int j, String a, String b, int dp[][]){
+        if(i<0) return j+1;
+        if(j<0) return i+1;
+
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        if(a.charAt(i)==b.charAt(j)){
+            return 0 + count(i-1, j-1, a, b, dp);
+        }
+        int del = 1 + count(i-1, j, a, b, dp);
+        int rep = 1 + count(i-1, j-1, a, b, dp);
+        int ins = 1 + count(i, j-1, a, b, dp);
+        
+        return dp[i][j] = Math.min(del, Math.min(rep, ins));
+    }
+}
