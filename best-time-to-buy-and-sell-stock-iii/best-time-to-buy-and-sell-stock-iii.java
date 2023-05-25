a8e5f59eb02472bb313d15da6e848caa9275bba9
aj7521 <43270371+aj7521@users.noreply.github.com>
2023-05-25 05:55:06 +0530

    Time: 190 ms (16.04%) | Memory: 79.4 MB (14.50%) - LeetSync

--- best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -2,34 +2,21 @@ class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
         int dp[][][] = new int[n+1][2][3];
-        int profit = 0;
-        for(int i=n-1; i>=0; i--){
-            for(int j=0; j<2; j++){
-                for(int k=1; k<=2; k++){
-                    if(j==1){
-                        int take = dp[i+1][0][k] - prices[i];
-                        int ntake = dp[i+1][1][k];
-                        profit = Math.max(take, ntake);
-                    }
-                    else{
-                        int take = dp[i+1][1][k-1] + prices[i];
-                        int ntake = dp[i+1][0][k];
-                        profit = Math.max(take, ntake);
-                    }
-                    dp[i][j][k] = profit;
-                }
+        for(int r[][]: dp){
+            for(int row[]: r){
+                Arrays.fill(row, -1);
             }
         }
-
-        return dp[0][1][2];
+        
+        return maxP(0, prices, 1, dp, 2);
     }
 
-    public int maxP(int i, int arr[], int buy, int dp[][], int k){
+    public int maxP(int i, int arr[], int buy, int dp[][][], int k){
         if(k==0) return 0;
         if(i==arr.length){
             return 0;
         }
-        if(dp[i][buy]!=-1) return dp[i][buy];
+        if(dp[i][buy][k]!=-1) return dp[i][buy][k];
         int profit = 0;
         if(buy==1){
             int take = maxP(i+1, arr, 0, dp, k) - arr[i];
@@ -41,6 +28,6 @@ class Solution {
             int ntake = maxP(i+1, arr, 0, dp, k);
             profit = Math.max(take, ntake);
         }
-        return profit;
+        return dp[i][buy][k] = profit;
     }
 }
