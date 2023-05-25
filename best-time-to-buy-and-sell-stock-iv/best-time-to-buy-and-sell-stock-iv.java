dd67385a1d2dddf0963d5fc902f10e4c4cc7c024
aj7521 <43270371+aj7521@users.noreply.github.com>
2023-05-25 06:17:46 +0530

    Time: 9 ms (41.71%) | Memory: 43.5 MB (20.32%) - LeetSync

---
+++ best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+    int n = prices.length;
+        int dp[][][] = new int[n+1][2][k+1];
+        for(int r[][]: dp){
+            for(int row[]: r){
+                Arrays.fill(row, -1);
+            }
+        }
+        
+        return maxP(0, prices, 1, dp, k);
+    }
+
+    public int maxP(int i, int arr[], int buy, int dp[][][], int k){
+        if(k==0) return 0;
+        if(i==arr.length){
+            return 0;
+        }
+        if(dp[i][buy][k]!=-1) return dp[i][buy][k];
+        int profit = 0;
+        if(buy==1){
+            int take = maxP(i+1, arr, 0, dp, k) - arr[i];
+            int ntake = maxP(i+1, arr, 1, dp, k);
+            profit = Math.max(take, ntake);
+        }
+        else{
+            int take = maxP(i+1, arr, 1, dp, k-1) + arr[i];
+            int ntake = maxP(i+1, arr, 0, dp, k);
+            profit = Math.max(take, ntake);
+        }
+        return dp[i][buy][k] = profit;
+    }
+}
