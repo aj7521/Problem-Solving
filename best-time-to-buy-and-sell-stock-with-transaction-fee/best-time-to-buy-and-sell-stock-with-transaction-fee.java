118bbe0824676ec8ffc8625f1933fb4096c80676
aj7521 <43270371+aj7521@users.noreply.github.com>
2023-05-25 06:45:37 +0530

    Time: 50 ms (14.52%) | Memory: 63.9 MB (5.03%) - LeetSync

---
+++ best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+     int n = prices.length;
+        int dp[][] = new int[n+1][2];
+        for(int rows[]: dp){
+            Arrays.fill(rows, -1);
+        }
+        return maxP(0, prices, 1, dp, fee);
+        // int profit = 0;
+        // for(int i=n-1; i>=0; i--){
+        //     for(int j=0; j<2; j++){
+        //         if(j==1){
+        //             int take = dp[i+1][0] - prices[i];
+        //             int ntake = dp[i+1][1];
+        //             profit = Math.max(take, ntake);
+        //         }
+        //         else{
+        //             int take = dp[i+2][1] + prices[i];
+        //             int ntake = dp[i+1][0];
+        //             profit = Math.max(take, ntake);
+        //         }
+        //         dp[i][j] = profit;
+        //     }
+        // }
+        // return dp[0][1];
+    }
+
+    public int maxP(int i, int arr[], int buy, int dp[][], int fee){
+        if(i==arr.length){
+            return 0;
+        }
+        if(dp[i][buy]!=-1) return dp[i][buy];
+        int profit = 0;
+        if(buy==1){
+            int take = maxP(i+1, arr, 0, dp, fee) - arr[i];
+            int ntake = maxP(i+1, arr, 1, dp, fee);
+            profit = Math.max(take, ntake);
+        }
+        else{
+            int take = maxP(i+1, arr, 1, dp, fee) + arr[i] - fee;
+            int ntake = maxP(i+1, arr, 0, dp, fee);
+            profit = Math.max(take, ntake);
+        }
+        return dp[i][buy] = profit;
+    }
+}
