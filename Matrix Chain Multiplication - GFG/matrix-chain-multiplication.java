7ca1e08f2aa0c3e504bd963c2b23380c31fd6d97
aj7521 <43270371+aj7521@users.noreply.github.com>
2023-05-27 06:15:00 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.java
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.java
@@ -32,11 +32,24 @@ class Solution{
     static int matrixMultiplication(int N, int arr[])
     {
         // code here
+        int n = N;
         int dp[][] = new int[N+1][N+1];
-        for(int row[]: dp){
-            Arrays.fill(row, -1);
+        for(int i=0; i<=n; i++){
+            dp[i][i] = 0;
         }
-        return findP(1, N-1, arr, dp);
+        for(int i=n-1; i>=1; i--){
+            for(int j=i+1; j<n; j++){
+                int mini = Integer.MAX_VALUE;
+                for(int k=i; k<=j-1; k++){
+                    int left = dp[i][k];
+                    int right = dp[k+1][j];
+                    int sum = arr[i-1] * arr[k] * arr[j] + left + right;
+                    mini = Math.min(mini, sum);
+                }
+                dp[i][j] = mini;
+            }
+        }
+        return dp[1][n-1];
     }
     
     public static int findP(int i, int j, int arr[], int dp[][]){
