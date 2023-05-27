7ac7e9a169eb51197f0ac677a0f9db72ffe11c92
aj7521 <43270371+aj7521@users.noreply.github.com>
2023-05-27 16:20:34 +0530

    Time: 12 ms (80.21%) | Memory: 41.3 MB (99.13%) - LeetSync

--- minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
+++ minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
@@ -10,10 +10,18 @@ class Solution {
         }
         Arrays.sort(arr);
         int dp[][] = new int[arr.length][arr.length];
-        for(int row[]: dp){
-            Arrays.fill(row, -1);
+        for(int i=c; i>=1; i--){
+            for(int j=1; j<=c; j++){
+                if(i>j) continue;
+                int min = Integer.MAX_VALUE;
+                for(int ind=i; ind<=j; ind++){
+                    int cost = arr[j+1] - arr[i-1] + dp[i][ind-1] + dp[ind+1][j];
+                    min = Math.min(min, cost);
+                }
+                dp[i][j] = min;
+            }
         }
-        return f(1, c, arr, dp);
+        return dp[1][c];
     }
 
     public int f(int i, int j, int arr[], int dp[][]){
@@ -21,7 +29,8 @@ class Solution {
         int min = Integer.MAX_VALUE;
         if(dp[i][j]!=-1) return dp[i][j];
         for(int ind=i; ind<=j; ind++){
-            int cost = arr[j+1] - arr[i-1] + f(i, ind-1, arr, dp) + f(ind+1, j, arr, dp);
+            int len = arr[j+1] - arr[i-1];
+            int cost =  len + f(i, ind-1, arr, dp) + f(ind+1, j, arr, dp);
             min = Math.min(min, cost);
         }
         return dp[i][j] = min;
