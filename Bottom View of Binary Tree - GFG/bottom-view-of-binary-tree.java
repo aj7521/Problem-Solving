758bedb2bf16ba7b62592e0994990ac8c91b326f
aj7521 <43270371+aj7521@users.noreply.github.com>
2023-06-02 00:54:09 +0530

    Added solution - LeetHub

--- Bottom View of Binary Tree - GFG/bottom-view-of-binary-tree.java
+++ Bottom View of Binary Tree - GFG/bottom-view-of-binary-tree.java
@@ -122,34 +122,36 @@ class Solution
     //Function to return a list containing the bottom view of the given tree.
     public ArrayList <Integer> bottomView(Node root)
     {
+        // Code here
         ArrayList<Integer> ans = new ArrayList<>();
+        if(root==null) return ans;
         TreeMap<Integer, Integer> map = new TreeMap<>();
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(new TreeNode(root, 0));
-        while(!q.isEmpty())
-        {
-            TreeNode t = q.poll();
-            Node n = t.node;
-            int val = n.data;
-            int x = t.x;
-            map.put(x, val);
-            if(n.left!=null) q.offer(new TreeNode(n.left, x-1));
-            if(n.right!=null) q.offer(new TreeNode(n.right, x+1));
+        Queue<Pair> q = new LinkedList<>();
+        q.offer(new Pair(root, 0));
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i=0; i<size; i++){
+                Pair p = q.poll();
+                Node temp = p.node;
+                int x = p.x;
+                map.put(x, temp.data);
+                if(temp.left!=null) q.offer(new Pair(temp.left, x-1));
+                if(temp.right!=null) q.offer(new Pair(temp.right, x+1));
+            }
         }
-        for(Integer mp: map.values())
-        {
-            ans.add(mp);
+        
+        for(Integer it: map.values()){
+            ans.add(it);
         }
         return ans;
     }
 }
 
-class TreeNode{
+class Pair{
     Node node;
     int x;
-    TreeNode(Node n, int a)
-    {
+    Pair(Node n, int xa){
         node = n;
-        x = a;
+        x = xa;
     }
 }
