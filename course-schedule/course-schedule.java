92450f2d568d3163775a5823cd1264451b1d9123
aj7521 <43270371+aj7521@users.noreply.github.com>
2023-06-03 15:57:40 +0530

    Time: 3 ms (94.95%) | Memory: 43.7 MB (30.65%) - LeetSync

--- course-schedule/course-schedule.java
+++ course-schedule/course-schedule.java
@@ -1,39 +1,43 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        //Making of indegree
-        int indegree[] = new int[numCourses];
-        //Making of adj
+    public boolean canFinish(int numCourses, int[][] edges) {
+        int n = numCourses;
         List<List<Integer>> adj = new ArrayList<>();
-        for(int i=0; i<numCourses; i++){
+        for(int i=0; i<n; i++){
             adj.add(new ArrayList<>());
         }
-        //Marking indegree and adj
-        for(int i=0; i<prerequisites.length; i++){
-            int u = prerequisites[i][0];
-            int v = prerequisites[i][1];
-            indegree[v]++;
-            adj.get(u).add(v);
-        } 
-        Queue<Integer> q = new LinkedList<>();
-        for(int i=0; i<numCourses; i++){
-            if(indegree[i]==0){
-                q.offer(i);
-            }
+        for(int i=0; i<edges.length; i++)
+        {
+            adj.get(edges[i][0]).add(edges[i][1]);
         }
-        List<Integer> ans = new ArrayList<>();
-        while(!q.isEmpty()){
-            int temp = q.poll();
-            ans.add(temp);
-            for(Integer i: adj.get(temp)){
-                indegree[i]--;
-                if(indegree[i]==0){
-                    q.offer(i);
+        boolean vis[] = new boolean[n];
+        boolean path[] = new boolean[n];
+        
+        for(int i=0; i<n; i++){
+            if(!vis[i]){
+                boolean check = dfs(i, adj, vis, path);
+                if(check){
+                    return false;
                 }
             }
         }
-        if(ans.size()==numCourses){
-            return true;
+        return true;
+    }
+
+    public static boolean dfs(int i, List<List<Integer>> adj, boolean vis[], boolean path[]){
+        vis[i] = true;
+        path[i] = true;
+        for(Integer n: adj.get(i)){
+            if(!vis[n]){
+                if(dfs(n, adj, vis, path)){
+                    return true;
+                }
+                
+            }
+            else if(path[n]){
+                return true;
+            }
         }
+        path[i] = false;
         return false;
     }
 }
