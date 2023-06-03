0abc467de20f3d80a4da8193731dad5cdd4991bd
aj7521 <43270371+aj7521@users.noreply.github.com>
2023-06-03 17:43:18 +0530

    Time: 95 ms (61.11%) | Memory: 45.4 MB (47.23%) - LeetSync

--- word-ladder/word-ladder.java
+++ word-ladder/word-ladder.java
@@ -1,52 +1,46 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         Set<String> set = new HashSet<>();
-        for(String s: wordList)
-        {
-            set.add(s);
+        for(int i=0; i<wordList.size(); i++){
+            set.add(wordList.get(i));
+        }
+        if(!set.contains(endWord)){
+            return 0;
         }
         set.remove(beginWord);
-        if(!set.contains(endWord)) return 0;
         Queue<Pair> q = new LinkedList<>();
         q.offer(new Pair(beginWord, 1));
-        int max = 0;
-        while(!q.isEmpty())
-        {
+        while(!q.isEmpty()){
             Pair p = q.poll();
-            String str = p.s;
-            char ch [] = new char[str.length()];
-            for(int i=0; i<ch.length; i++)
-            {
-                ch[i] = str.charAt(i);
+            String temp = p.s;
+            int val = p.val;
+            if(temp.equals(endWord)){
+                return val;
             }
-            int c = p.c;
-            if(str.equals(endWord)) return c;
-            for(int i=0; i<ch.length; i++)
-            {
-                for(int j=0; j<26; j++)
-                {
-                    ch[i] = (char)(97+j);
+            char ch[] = new char[temp.length()];
+            for(int i=0; i<temp.length(); i++){
+                ch[i] = temp.charAt(i);
+            }
+            for(int i=0; i<temp.length(); i++){
+                for(int j=0; j<26; j++){
+                    ch[i] = (char) (j+97);
                     String check = new String(ch);
-                    
-                    if(!check.equals(str) && set.contains(check))
-                    {
+                    if(!check.equals(temp) && set.contains(check)){
                         set.remove(check);
-                        q.offer(new Pair(check, c+1));
+                        q.offer(new Pair(check, val+1));
                     }
                 }
-                ch[i] = str.charAt(i);
+                ch[i] = temp.charAt(i);
             }
         }
         return 0;
     }
 }
-
 class Pair{
     String s;
-    int c;
-    Pair(String str, int cnt)
-    {
+    int val;
+    Pair(String str, int i){
         s = str;
-        c = cnt;
+        val = i;
     }
 }
