3f254cb87f32b2e246fabae1089fab19d351abb6
aj7521 <43270371+aj7521@users.noreply.github.com>
2023-06-03 12:29:19 +0530

    Time: 6 ms (25.45%) | Memory: 50.5 MB (73.13%) - LeetSync

--- number-of-islands/number-of-islands.java
+++ number-of-islands/number-of-islands.java
@@ -8,24 +8,41 @@ class Solution {
             for(int j=0; j<m; j++){
                 if(!vis[i][j] && grid[i][j]=='1'){
                     count++;
-                    dfs(i, j, n, m, grid, vis);
+                    bfs(i, j, n, m, grid, vis);
                 }
             }
         }
         return count;
     }
 
-    public void dfs(int i, int j, int n, int m, char grid[][], boolean vis[][]){
+    public void bfs(int i, int j, int n, int m, char grid[][], boolean vis[][]){
         vis[i][j] = true;
         int x[] = {-1,0,1,0};
         int y[] = {0,-1,0,1};
-        for(int r=0; r<4; r++){
-            int xa = x[r] + i;
-            int ya = y[r] + j;
-            if(xa>=0 && xa<n && ya>=0 && ya<m && grid[xa][ya]=='1' && !vis[xa][ya]){
-                dfs(xa, ya, n, m, grid, vis);
+        Queue<Pair> q = new LinkedList<>();
+        q.offer(new Pair(i, j));
+        while(!q.isEmpty()){
+            Pair p = q.poll();
+            int r = p.r;
+            int c = p.c;
+            for(int k=0; k<4; k++){
+                int xa = x[k] + r;
+                int ya = y[k] + c;
+                if(xa>=0 && xa<n && ya>=0 && ya<m && !vis[xa][ya] && grid[xa][ya]=='1'){
+                    vis[xa][ya] = true;
+                    q.offer(new Pair(xa, ya));
+                }
             }
         }
         return;
+    }
+}
+
+class Pair{
+    int r;
+    int c;
+    Pair(int ra, int ca){
+        r = ra;
+        c = ca;
     }
 }
