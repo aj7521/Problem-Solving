fbae489be5e7b2076ba2e298bb3fbae62e39ce64
aj7521 <43270371+aj7521@users.noreply.github.com>
2023-06-04 00:59:44 +0530

    Time: 1 ms (99.43%) | Memory: 43.8 MB (24.63%) - LeetSync

--- minimum-falling-path-sum/minimum-falling-path-sum.java
+++ minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,36 +1,28 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
         int n = matrix.length;
+        int min = Integer.MAX_VALUE;
         int dp[][] = new int [n][n];
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(i==0) dp[0][j] = matrix[0][j];
-                else{
-                    int ls = Integer.MAX_VALUE;
-                    int rs = Integer.MAX_VALUE;
-                    int ds = Integer.MAX_VALUE;
-                    if(j>0) ls = dp[i-1][j-1];
-                    ds = dp[i-1][j];
-                    if(j<n-1) rs = dp[i-1][j+1];
-                    dp[i][j] = Math.min(Math.min(ls, ds), rs) + matrix[i][j];
-                }
-            }
+        for(int row[]: dp){
+            Arrays.fill(row, -1);
         }
-        int max = dp[n-1][0];
-        for(int j=1; j<n; j++){
-            max = Math.min(max, dp[n-1][j]);
+        for(int j=0; j<n; j++){
+            
+            min = Math.min(min, count(0, j, n, matrix, dp));
         }
-        return max;
+        return min;
     }
 
-    // public int count(int i, int j, int n, int matrix[][], int dp[][]){
-    //     if(j<0 || j>=n) return Integer.MAX_VALUE;
-    //     if(i==0) return matrix[0][j];
-    //     if(dp[i][j]!=-1) return dp[i][j];
-    //     int ls = count(i-1, j-1, n, matrix, dp);
-    //     int ds = count(i-1, j, n, matrix, dp);
-    //     int rs = count(i-1, j+1, n, matrix, dp);
+    public int count(int i, int j, int n, int arr[][], int dp[][]){
+        if(i==n-1) return arr[i][j];
 
-    //     return dp[i][j] = Math.min(Math.min(ls, ds), rs) + matrix[i][j];
-    // }
+        int left=Integer.MAX_VALUE;
+        int right=Integer.MAX_VALUE;
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        if(j-1>=0) left = count(i+1, j-1, n, arr, dp);
+        if(j+1<n) right = count(i+1, j+1, n, arr, dp);
+        int down = count(i+1, j, n, arr, dp);
+        return dp[i][j] = Math.min(left, Math.min(down, right)) + arr[i][j];
+    }
 }
