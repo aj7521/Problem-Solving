e61eef55b801871021a61e3c3c1a0cbe747f92f3
aj7521 <43270371+aj7521@users.noreply.github.com>
2023-06-04 02:10:36 +0530

    Time: 36 ms (39.41%) | Memory: 44.7 MB (18.47%) - LeetSync

--- cherry-pickup-ii/cherry-pickup-ii.java
+++ cherry-pickup-ii/cherry-pickup-ii.java
@@ -1,74 +1,40 @@
 class Solution {
     public int cherryPickup(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int dp[][][] = new int[m][n][n];
-        return count(m, n, grid, dp);
-    }
-
-    public int count(int m, int n, int grid[][], int dp[][][]){
-        for(int i=m-1; i>=0; i--){
-            for(int j1=0; j1<n; j1++){
-                for(int j2=0; j2<n; j2++){
-                    if(i==m-1){
-                        if(j1==j2){
-                            dp[i][j1][j2] = grid[i][j1];
-                        }
-                        else
-                        {
-                            dp[i][j1][j2] = grid[i][j1] + grid[i][j2];
-                        }
-                    }
-                    else
-                    {
-                        int max = (int)(-1e8);
-                        for(int x=-1; x<=1; x++){
-                            for(int y=-1; y<=1; y++){
-                                int val = 0;
-                                if(j1==j2){
-                                    val = grid[i][j1];
-                                }
-                                else{
-                                    val = grid[i][j1] + grid[i][j2];
-                                }
-                                if(j1+x>=0 && j1+x<n && j2+y>=0 && j2+y<n) val += dp[i+1][j1+x][j2+y];
-                                max = Math.max(max, val);
-                            }
-                        }
-                        dp[i][j1][j2] = max;
-                    }
-                }
+        int n = grid.length;
+        int m = grid[0].length;
+        int dp[][][] = new int [n][m][m];
+        for(int arr[][]: dp){
+            for(int row[]: arr){
+                Arrays.fill(row, -1);
             }
         }
-        return dp[0][0][n-1];
+        return count(0, 0, m-1, n, m, grid, dp);
     }
 
-    // public int count(int i, int j1, int j2, int m, int n, int grid[][], int dp[][][]){
-    //     if(j1<0 || j1>=n || j2<0 || j2>=n) return (int)(-1e8);
-    //     if(i==m-1){
-    //         if(j1==j2){
-    //             return grid[i][j1];
-    //         }
-    //         else
-    //         {
-    //             return grid[i][j1] + grid[i][j2];
-    //         }
-    //     }
-    //     if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
-    //     int max = (int)(-1e8);
-    //     for(int x=-1; x<=1; x++){
-    //         for(int y=-1; y<=1; y++){
-    //             int val = 0;
-    //             if(j1==j2){
-    //                 val = grid[i][j1];
-    //             }
-    //             else{
-    //                 val = grid[i][j1] + grid[i][j2];
-    //             }
-    //             val += count(i+1, j1+x, j2+y, m, n, grid, dp);
-    //             max = Math.max(max, val);
-    //         }
-    //     }
-    //     return dp[i][j1][j2] = max;
-    // }
+    public int count(int i, int j1, int j2, int n, int m, int grid[][], int dp[][][]){
+        if(j1<0 || j2<0 || j1>=m || j2>=m) return (int)(-1e8);
+        if(i==n-1){
+            if(j1==j2){
+                return grid[i][j1];
+            }
+            else{
+                return grid[i][j1] + grid[i][j2];
+            }
+        }
+        if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
+        int max = (int)(-1e8);
+        for(int k=-1; k<=1; k++){
+            for(int l=-1; l<=1; l++){
+                int val = 0;
+                if(j1==j2){
+                    val = grid[i][j1];
+                }else{
+                    val = grid[i][j1] + grid[i][j2];
+                } 
+                val += count(i+1, j1+k, j2+l, n, m, grid, dp);
+                max = Math.max(max, val);
+            }
+        }
+        return dp[i][j1][j2] = max;
+    }
 }
