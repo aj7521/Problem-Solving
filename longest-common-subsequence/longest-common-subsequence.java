0c83598c4f930f326483778941e02628dfd625d9
aj7521 <43270371+aj7521@users.noreply.github.com>
2023-06-04 05:04:11 +0530

    Time: 44 ms (16.01%) | Memory: 48.8 MB (72.59%) - LeetSync

--- longest-common-subsequence/longest-common-subsequence.java
+++ longest-common-subsequence/longest-common-subsequence.java
@@ -1,40 +1,20 @@
 class Solution {
-    public int longestCommonSubsequence(String a, String b) {
-        int n = a.length();
-        int m = b.length();
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n = text1.length();
+        int m = text2.length();
         int dp[][] = new int[n+1][m+1];
-        // for(int row[]: dp){
-        //     Arrays.fill(row, -1);
-        // }
-        // return lcs(n-1, m-1, a, b, dp);
-        for(int i=0; i<=n; i++){
-            dp[i][0] = 0;
+        for(int row[]: dp){
+            Arrays.fill(row, -1);
         }
-        for(int j=0; j<=m; j++){
-            dp[0][j] = 0;
-        }
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++){
-                if(a.charAt(i-1)==b.charAt(j-1)){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                }
-                else
-                {
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-                }
-            }
-        }
-        return dp[n][m];
+        return count(n, m, text1, text2, dp);
     }
 
-    public int lcs(int i, int j, String str, String srt, int dp[][]){
-        if(i<0 || j<0){
-            return 0;
-        }
+    public int count(int i, int j, String s, String r, int dp[][]){
+        if(i==0 || j==0) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        if(str.charAt(i)==srt.charAt(j)){
-            return 1 + lcs(i-1, j-1, str, srt, dp);
+        if(s.charAt(i-1)==r.charAt(j-1)){
+            return 1 + count(i-1, j-1, s, r, dp);
         }
-        return dp[i][j] = Math.max(lcs(i-1, j, str, srt, dp), lcs(i, j-1, str, srt, dp));
+        return dp[i][j] = Math.max(count(i, j-1, s, r, dp), count(i-1, j, s, r, dp));
     }
 }
