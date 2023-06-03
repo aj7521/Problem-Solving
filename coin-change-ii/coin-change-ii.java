c6bb693130135be2f422505cb09a8fa4f493869d
aj7521 <43270371+aj7521@users.noreply.github.com>
2023-06-04 04:56:54 +0530

    Time: 6 ms (62.20%) | Memory: 46.1 MB (51.58%) - LeetSync

--- coin-change-ii/coin-change-ii.java
+++ coin-change-ii/coin-change-ii.java
@@ -1,39 +1,25 @@
 class Solution {
-    public int change(int tar, int[] arr) {
-        int dp[][] = new int[arr.length][tar+1];
-        int n = arr.length;
-        // for(int row[]: dp){
-        //     Arrays.fill(row, -1);
-        // }
-        // int val = count(n-1, tar, arr, dp);
-        // return val;
-
-        for(int i=0; i<=tar; i++){
-            if(i%arr[0]==0) dp[0][i] = 1;
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+        int dp[][] = new int[n][amount+1];
+        for(int row[]: dp){
+            Arrays.fill(row, -1);
         }
-        
-
-        for(int i=1; i<n; i++){
-            for(int j=0; j<=tar; j++){
-                int np = dp[i-1][j];
-                int p = 0;
-                if(arr[i]<=j) p = dp[i][j-arr[i]];
-                dp[i][j] = np + p;
-            }
-        }
-        return dp[n-1][tar];
+        return count(n-1, coins, amount, dp);
     }
 
-    public int count(int i, int t, int arr[], int dp[][]){
+    public int count(int i, int arr[], int sum, int dp[][]){
         if(i==0){
-            if(t%arr[i]==0) return 1;
-            else return 0;
+            if(sum%arr[i]==0){
+                return 1;
+            }
+            return 0;
         }
-        if(dp[i][t]!=-1) return dp[i][t];
+        if(dp[i][sum]!=-1) return dp[i][sum];
+        int nt = count(i-1, arr, sum, dp);
+        int t = 0;
+        if(arr[i]<=sum) t = count(i, arr, sum-arr[i], dp);
 
-        int np = count(i-1, t, arr, dp);
-        int p = 0;
-        if(arr[i]<=t) p = count(i, t-arr[i], arr, dp);
-        return dp[i][t] = np + p;
+        return dp[i][sum] = t + nt;
     }
 }
