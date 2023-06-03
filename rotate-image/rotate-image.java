d33c64f3034cec5ee6e000248069de0fcd10d98d
aj7521 <43270371+aj7521@users.noreply.github.com>
2023-06-03 21:00:25 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (93.85%) - LeetSync

---
+++ rotate-image/rotate-image.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        //transpose
+        for(int i=0; i<n; i++){
+            for(int j=i; j<m; j++){
+                if(i==j) continue;
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        //reverse
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m/2; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][m-j-1];
+                matrix[i][m-j-1] = temp;
+            }
+        }
+        return;
+    }
+}
