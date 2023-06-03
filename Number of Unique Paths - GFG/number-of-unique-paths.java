9e2431299dc3f6ff59786645b0e67d1988414b9d
aj7521 <43270371+aj7521@users.noreply.github.com>
2023-06-03 22:10:46 +0530

    Added solution - LeetHub

--- Number of Unique Paths - GFG/number-of-unique-paths.java
+++ Number of Unique Paths - GFG/number-of-unique-paths.java
@@ -37,19 +37,17 @@ class Solution
     {
         //Your code here
         int dp[][] = new int[a][b];
-        for(int row[]: dp){
-            Arrays.fill(row, -1);
+        for(int i=0; i<a; i++){
+            for(int j=0; j<b; j++){
+                if(i==0 && j==0) dp[i][j] = 1;
+                else{
+                    int left = 0, up = 0;
+                    if(j>0) left = dp[i][j-1];
+                    if(i>0) up = dp[i-1][j];
+                    dp[i][j] = left + up;
+                }
+            }
         }
-        return count(a-1, b-1, dp);
-    }
-    
-    public static int count(int i, int j, int dp[][]){
-        if(i==0 && j==0) return 1;
-        if(i<0 || j<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int left = count(i, j-1, dp);
-        int up = count(i-1, j, dp);
-        
-        return dp[i][j] = left + up;
+        return dp[a-1][b-1];
     }
 }
