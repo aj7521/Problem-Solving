f4744688c22dc9e161cc906d6f1e6ab90e8cb93f
aj7521 <43270371+aj7521@users.noreply.github.com>
2023-06-04 17:13:54 +0530

    Time: 40 ms (30.14%) | Memory: 44.4 MB (10.27%) - LeetSync

--- minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
+++ minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -7,10 +7,21 @@ class Solution {
         }
         String r = new String(ch);
         int dp[][] = new int[n+1][n+1];
-        for(int row[]: dp){
-            Arrays.fill(row, -1);
+        for(int i=0; i<n; i++){
+            dp[i][0] = 0;
+            dp[0][i] = 0;
         }
-        int lcs = count(n, n, s, r, dp);
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(s.charAt(i-1)==r.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
+                }
+            }
+        }
+        int lcs = dp[n][n];
         return n - lcs;
     }
 
