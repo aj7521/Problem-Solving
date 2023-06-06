49c97e99043868ab20d87db94c97aaca9a16f7ed
aj7521 <43270371+aj7521@users.noreply.github.com>
2023-06-06 17:44:05 +0530

    Time: 2 ms (100.00%) | Memory: 43.3 MB (31.58%) - LeetSync

---
+++ add-two-numbers-ii/add-two-numbers-ii.java
@@ -0,0 +1,78 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        l1 = reverse(l1);
+        l2 = reverse(l2);
+
+        ListNode head = null;
+        ListNode cur = null;
+        int carry = 0;
+        while(l1!=null && l2!=null){
+            int value = l1.val + l2.val + carry;
+            carry = value / 10;
+            value = value % 10;
+
+            ListNode node = new ListNode(value);
+            if(head==null) {
+                head = node;
+                cur = node;
+            }
+            else{
+                cur.next = node;
+                cur = node;
+            }
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        while(l1!=null){
+            int value = l1.val + carry;
+            
+            carry = value / 10;
+            value = value % 10;
+            ListNode node = new ListNode(value);
+            cur.next = node;
+            cur = node;
+            l1 = l1.next;
+        }
+        while(l2!=null){
+            int value = l2.val + carry;
+            
+            carry = value / 10;
+            value = value % 10;
+            ListNode node = new ListNode(value);
+            cur.next = node;
+            cur = node;
+            l2 = l2.next;
+        }
+        if(carry>0){
+            ListNode node = new ListNode(carry);
+            cur.next = node;
+            cur = node;
+        }
+
+        ListNode res = reverse(head);
+        return res;
+    }
+
+    public ListNode reverse(ListNode head){
+        ListNode cur = head;
+        ListNode next = cur;
+        ListNode prev = null;
+        while(next!=null){
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+}
