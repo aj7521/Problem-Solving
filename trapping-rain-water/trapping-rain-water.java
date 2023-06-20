6272880635362d203a7b485334e7393c3de27b74
aj7521 <43270371+aj7521@users.noreply.github.com>
2023-06-21 01:07:12 +0530

    Time: 1 ms (96.93%) | Memory: 44.4 MB (36.86%) - LeetSync

--- trapping-rain-water/trapping-rain-water.java
+++ trapping-rain-water/trapping-rain-water.java
@@ -1,30 +1,32 @@
 class Solution {
     public int trap(int[] arr) {
         int n = arr.length;
-        int prefix[] = new int[n];
-        int suffix[] = new int[n];
-
-        //prefix
-        int max = 0;
-        for(int i=0; i<n; i++){
-            max = Math.max(max, arr[i]);
-            prefix[i] = max;
-        }
-
-        //suffix
-        max = 0;
-        for(int i=n-1; i>=0; i--){
-            max = Math.max(max, arr[i]);
-            suffix[i] = max;
-        }
-
+        int l = 0, r = n-1;
+        int maxl = arr[0];
+        int maxr = arr[n-1];
         int sum = 0;
-        for(int i=0; i<n; i++){
-            int min = Math.min(prefix[i], suffix[i]);
-            int val = min - arr[i];
-            if(val>0){
-                sum += val;
+        while(l<r){
+            if(arr[l]<=arr[r]){
+                if(arr[l]>maxl){
+                    maxl = arr[l];
+                }
+                if(maxl - arr[l] > 0){
+                    sum += maxl - arr[l];
+                }
+                
+                l++;
             }
+            else{
+                if(arr[r]>maxr){
+                    maxr = arr[r];
+                }
+                if(maxr - arr[r] > 0){
+                    sum += maxr - arr[r];
+                }
+                
+                r--;
+            }
+
         }
         return sum;
     }
