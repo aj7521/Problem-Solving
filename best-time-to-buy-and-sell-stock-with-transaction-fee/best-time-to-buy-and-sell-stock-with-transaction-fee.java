e901f647103e7408997093af14ee441043913385
aj7521 <43270371+aj7521@users.noreply.github.com>
2023-06-22 10:59:18 +0530

    Time: 44 ms (21.02%) | Memory: 63.7 MB (13.42%) - LeetSync

--- best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,45 +1,28 @@
 class Solution {
     public int maxProfit(int[] prices, int fee) {
-     int n = prices.length;
-        int dp[][] = new int[n+1][2];
-        for(int rows[]: dp){
-            Arrays.fill(rows, -1);
+        int n = prices.length;
+        int dp[][] = new int[n][2];
+        for(int row[]: dp){
+            Arrays.fill(row, -1);
         }
-        return maxP(0, prices, 1, dp, fee);
-        // int profit = 0;
-        // for(int i=n-1; i>=0; i--){
-        //     for(int j=0; j<2; j++){
-        //         if(j==1){
-        //             int take = dp[i+1][0] - prices[i];
-        //             int ntake = dp[i+1][1];
-        //             profit = Math.max(take, ntake);
-        //         }
-        //         else{
-        //             int take = dp[i+2][1] + prices[i];
-        //             int ntake = dp[i+1][0];
-        //             profit = Math.max(take, ntake);
-        //         }
-        //         dp[i][j] = profit;
-        //     }
-        // }
-        // return dp[0][1];
+        return profit(0, 1, prices, fee, dp);
     }
 
-    public int maxP(int i, int arr[], int buy, int dp[][], int fee){
+    public int profit(int i, int buy, int arr[], int fees, int dp[][]){
         if(i==arr.length){
             return 0;
         }
         if(dp[i][buy]!=-1) return dp[i][buy];
         int profit = 0;
         if(buy==1){
-            int take = maxP(i+1, arr, 0, dp, fee) - arr[i];
-            int ntake = maxP(i+1, arr, 1, dp, fee);
-            profit = Math.max(take, ntake);
+            int take = profit(i+1, 0, arr, fees, dp) - arr[i];
+            int nottake = profit(i+1, 1, arr, fees, dp);
+            profit = Math.max(take, nottake);
         }
         else{
-            int take = maxP(i+1, arr, 1, dp, fee) + arr[i] - fee;
-            int ntake = maxP(i+1, arr, 0, dp, fee);
-            profit = Math.max(take, ntake);
+            int take = profit(i+1, 1, arr, fees, dp) + arr[i] - fees;
+            int nottake = profit(i+1, 0, arr, fees, dp);
+            profit = Math.max(take, nottake);
         }
         return dp[i][buy] = profit;
     }
