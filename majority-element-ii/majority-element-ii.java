bfc7b191d51eae3404b0fafb1f5f44fd8e9b1b97
aj7521 <43270371+aj7521@users.noreply.github.com>
2023-07-02 07:04:38 +0530

    Time: 2 ms (98.00%) | Memory: 47.4 MB (9.90%) - LeetSync

--- majority-element-ii/majority-element-ii.java
+++ majority-element-ii/majority-element-ii.java
@@ -1,17 +1,47 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         List<Integer> ans = new ArrayList<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
+        int ele1 = Integer.MIN_VALUE;
+        int ele2 = Integer.MIN_VALUE;
+        int count1 = 0;
+        int count2 = 0;
         for(int i=0; i<nums.length; i++){
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-        }
-        int maj = nums.length / 3;
-        for(Map.Entry<Integer, Integer> mp: map.entrySet()){
-            int key = mp.getKey();
-            int val = mp.getValue();
-            if(val > maj){
-                ans.add(key);
+            if(count1==0 && ele2!=nums[i]){
+                ele1 = nums[i];
+                count1 = 1;
             }
+            else if(count2==0 && ele1!=nums[i]){
+                ele2 = nums[i];
+                count2 = 1;
+            }
+            else if(ele1 == nums[i]){
+                count1++;
+            }
+            else if(ele2 == nums[i]){
+                count2++;
+            }
+            else{
+                count1--;
+                count2--;
+            }
+        }
+        //checking
+        count1 = 0;
+        count2 = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == ele1){
+                count1++;
+            }
+            if(nums[i] == ele2){
+                count2++;
+            }
+        }
+
+        if(count1 > (nums.length/3)){
+            ans.add(ele1);
+        }
+        if(count2 > (nums.length/3)){
+            ans.add(ele2);
         }
         return ans;
     }
