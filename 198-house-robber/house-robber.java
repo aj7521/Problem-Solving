2f2d0eb79be86ec0df5413c0c4ebe1f9e0008da5
aj7521 <43270371+aj7521@users.noreply.github.com>
2024-04-21 19:00:49 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (69.88%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,16 +1,18 @@
 class Solution {
     public int rob(int[] nums) {
-        int dp[] = new int[nums.length];
+        int n = nums.length;
+        int[] dp = new int[n+1];
         Arrays.fill(dp, -1);
-        return maxMoney(nums.length-1, nums, dp);
+        return get(n-1, nums, dp);
     }
 
-    public int maxMoney(int i, int nums[], int dp[]){
+    public int get(int i, int arr[], int dp[]){
         if(i<0) return 0;
+        if(i==0) return arr[i];
         if(dp[i]!=-1) return dp[i];
-        int pick = nums[i] + maxMoney(i-2, nums, dp);
-        int notPick = maxMoney(i-1, nums, dp);
+        int np = get(i-1, arr, dp);
+        int p = arr[i] + get(i-2, arr, dp);
 
-        return dp[i] = Math.max(pick, notPick);
+        return dp[i] = Math.max(p, np);
     }
 }
